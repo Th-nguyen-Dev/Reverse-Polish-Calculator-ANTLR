6c745ba86dd37f5f6775128cabe9c42050b892a4
Th-nguyen-Dev <th.nguyen.developer@gmail.com>
2024-03-04 19:13:49 -0800

    Edit maven pom.xml to include dependency in the compilation of the package
    - Use plugin maven-shade-plugin
    - Add dependency for Antlr run-time

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,13 +1,14 @@
 //Add the lexer and parser as import
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
-public class Main {
+import java.io.*;
+public class MainClass  {
     //create a new instance of the lexer, and parser with a test string and print the parse tree
     public static void main(String[] args) throws Exception {
-        ExprLexer lexer = new ExprLexer(CharStreams.fromString("3+4*5"));
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        ExprParser parser = new ExprParser(tokens);
-        ParseTree tree = parser.prog();
+        ExprLexer lexer = new ExprLexer((CharStream) CharStreams.fromString("3+4*5"));
+        CommonTokenStream tokens = new CommonTokenStream((TokenSource) lexer);
+        ExprParser parser = new ExprParser((TokenStream) tokens);
+        ParseTree tree = parser.expr();
         System.out.println(tree.toStringTree(parser));
     }
 
