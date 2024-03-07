53789e5299966bc7734d1c9c16a6a6e511ae2828
Th-nguyen-Dev <th.nguyen.developer@gmail.com>
2024-03-07 13:29:04 -0800

    I got my build folder to run

---
+++ src/main/antlr/ExprVisitor.java
@@ -0,0 +1,24 @@
+// Generated from Expr.g4 by ANTLR 4.13.1
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link ExprParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface ExprVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link ExprParser#prog}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitProg(ExprParser.ProgContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ExprParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitExpr(ExprParser.ExprContext ctx);
+}
